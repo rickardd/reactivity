3cd073fcbf10911fa32b32ed70d9c0e87ab8e168
Rickard Dahlstrom <rickard@hnry.co.nz>
2022-10-13 14:20:00 +1300

    Implement test for utils

--- sandbox/source/tests/utils.test.js
+++ sandbox/source/tests/utils.test.js
@@ -1,3 +1,23 @@
-test('Init test', () => {
-  expect(true).toBe(true);
-});
+import { getElements } from "../utils.js";
+
+describe("utils.js", () => {
+
+  describe("getElements method", () => {
+    
+    it('gets 2 elements from the dom', () => {
+      document.body.innerHTML = `
+        <div class='my-elements'>My text</div>
+        <div class='my-elements'>My text 2</div>
+      `
+      const elements = getElements('.my-elements')
+      
+      // innerText not working, we use innerHTML instead
+      expect(elements[0].innerHTML).toBe('My text');
+      expect(elements[0].classList.contains('my-elements')).toBe(true);
+      expect(elements[0]).toBeDefined();
+      expect(elements.length).toBe(2);
+      expect(Array.isArray(elements)).toBeTruthy();
+    });
+  })
+  
+})
