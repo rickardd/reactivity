029f2f711fe16a7e433d70fb2ca7c0152a894522
Rickard Dahlstrom <rickard@hnry.co.nz>
2022-12-02 10:41:17 +1300

    Bind correct proxy to each component

--- sandbox/source/dom-events.js
+++ sandbox/source/dom-events.js
@@ -1,5 +1,3 @@
-import { getElements } from "./utils.js";
-
 const BINDINGS = [
   {
     selector: '[\\@click]',
@@ -39,18 +37,20 @@ const BINDINGS = [
 ]
 
 // Execute expression e.g @click='proxy.price += 10' or @input='proxy.price += 10'
-// Proxy needs to be available as it's used in the expression.
+// Proxy needs to be available as it's used in the eval() expression.
 const executeExpression = (el, proxy, attribute) => {
   const expression = el.attributes[attribute].value
-  
+
   // Quick and dirty. Eval is risky. 
   eval(expression)
 }
 
 // Bind all DOM elements.
-const bind = (proxy) => {
+const bind = (proxy, appEl) => {
   const bindElements = (selector, attribute, eventName) => {
-    getElements(selector).forEach( el => el.addEventListener(eventName, executeExpression.bind(this, el, proxy, attribute)) )
+    Array.from(appEl.querySelectorAll(selector)).forEach( el => {
+      el.addEventListener(eventName, executeExpression.bind(this, el, proxy, attribute)) 
+    })
   }
 
   BINDINGS.forEach(({selector, attribute, events}) => {
