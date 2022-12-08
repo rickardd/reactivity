96015997c925303f3c740ae31d498b74c3bf142c
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2022-12-08 15:54:51 +1300

    Move Reactive functions to a module

---
+++ sandbox/source/core/reactive.js
@@ -0,0 +1,27 @@
+import { render } from "../core/render.js";
+
+function Reactive(compute) {
+  const self = this
+  this.computeMap = new Map() // Holds computed method e.g {sum: fn() {..}, ...}
+
+  const proxyHandler = {
+    set(proxyMap, property, value) {
+      proxyMap.set(property, value) // Set values defined by proxy e.g proxy.price - (none computed values)
+      render(proxyMap, compute, self.computeMap)
+      return true // Fixes a proxy trap issue but why is this needed? What should it return?
+    },
+    get(proxyMap, property) {
+      return proxyMap.get(property)
+    },
+    deleteProperty(proxyMap, property) { // e.g delete proxy.names
+      // if (!(property in proxyMap)) { return false; }
+      // return proxyMap.removeItem(property);
+      console.log('deleteProperty()');
+      return true
+    },
+  }
+
+  return new Proxy(new Map, proxyHandler);
+}
+
+export { Reactive }
--- sandbox/source/index.js
+++ sandbox/source/index.js
@@ -1,31 +1,7 @@
 import { prepare as prepareTemplate } from './template-engine.js'
 import { bind as bindDomEvents } from "./dom-events.js";
 import { bind as bindModelEvents } from "./dom-model-events.js";
-import { render } from "./core/render.js";
-
-function Reactive(compute) {
-  const self = this
-  this.computeMap = new Map() // Holds computed method e.g {sum: fn() {..}, ...}
-
-  const proxyHandler = {
-    set(proxyMap, property, value) {
-      proxyMap.set(property, value) // Set values defined by proxy e.g proxy.price - (none computed values)
-      render(proxyMap, compute, self.computeMap)
-      return true // Fixes a proxy trap issue but why is this needed? What should it return?
-    },
-    get(proxyMap, property) {
-      return proxyMap.get(property)
-    },
-    deleteProperty(proxyMap, property) { // e.g delete proxy.names
-      // if (!(property in proxyMap)) { return false; }
-      // return proxyMap.removeItem(property);
-      console.log('deleteProperty()');
-      return true
-    },
-  }
-
-  return new Proxy(new Map, proxyHandler);
-}
+import { Reactive } from "./core/reactive.js";
 
 function Raccoon(appEl) {
   this.compute = {}
