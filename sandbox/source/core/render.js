1f69323e77adc7f179d2f2005b8dd49cec1abe71
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2023-01-20 10:16:10 +1300

    Improve compute check

--- sandbox/source/core/render.js
+++ sandbox/source/core/render.js
@@ -3,8 +3,8 @@ import { update as updateTemplate } from '../template/template-engine.js'
 import { updateInput as updateInputValue, updateModel as updateModelValue } from "../directives/input-value.js";
 import { update as updateFor } from "../directives/r-for.js";
 
-function render(proxy, compute = null) {    
-    if(compute) updateCompute(proxy, compute) // executes compute functions and stores the result on the proxy
+function render(proxy, compute = null) {  
+    if(compute && Object.entries(compute).length) updateCompute(proxy, compute) // executes compute functions and stores the result on the proxy
     updateInputValue(proxy) // Update <input :value="...">
     updateModelValue(proxy) // Update <input :r-model="...">
     updateTemplate(proxy) // Updates template properties {{myValue}}
