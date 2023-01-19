11750f73cc8fec350384fd791b3d3faa033ce87c
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2023-01-20 10:26:51 +1300

    Remove redundant line of code

--- sandbox/source/core/render.js
+++ sandbox/source/core/render.js
@@ -3,9 +3,9 @@ import { update as updateTemplate } from '../template/template-engine.js'
 import { updateInput as updateInputValue, updateModel as updateModelValue } from "../directives/input-value.js";
 import { update as updateFor } from "../directives/r-for.js";
 
-function render(proxy, compute = null) {  
+function render(proxy, compute) {  
     const hasCompute = compute && Object.entries(compute).length
-    
+
     if(hasCompute) updateCompute(proxy, compute) // executes compute functions and stores the result on the proxy
     updateInputValue(proxy) // Update <input :value="...">
     updateModelValue(proxy) // Update <input :r-model="...">
