a24838415e85a4ef9b0f307d3a359344e1f88e86
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2023-01-25 15:05:04 +1300

    Refactor r-for module

--- sandbox/source/directives/r-for.js
+++ sandbox/source/directives/r-for.js
@@ -3,10 +3,11 @@ import { DATA_BINDING_SELECTOR } from "../template/template-engine.js";
 
 const FOR_SELECTOR = '[r-for]'
 const FOR_ATTRIBUTE = 'r-for'
+const FOR_KEY = 'r-for-key'
 
 const rForMap = new Map()
 
-function getTemplate(value, el) {
+const getTemplate = (value, el) => {
   const uid = el.attributes["r-for-key"].value
 
   const template = rForMap.get(uid);
@@ -16,6 +17,16 @@ function getTemplate(value, el) {
   return template.outerHTML
 }
 
+const getParsedTemplate = (operator, target, el) => {
+  let template = ''
+  if (operator.toLowerCase() == 'of') {
+    for (const value of target) {
+      template += getTemplate(value, el)
+    }
+  }
+  return template
+}
+
 const update = (proxyMap) => {
   getElements(FOR_SELECTOR).forEach( el => {
     const property = el.attributes[FOR_ATTRIBUTE].value.replace('proxy.', '') // Handles both r-for="name in proxy.names" and r-for="name in names"
@@ -23,29 +34,20 @@ const update = (proxyMap) => {
     const target = proxyMap.get(targetProperty)
 
     // Store the innerHTML of r-for as a template
-    if (!el.attributes["r-for-key"]) {
+    if (!el.attributes[FOR_KEY]) {
       const uid = createUid();
-      el.setAttribute("r-for-key", uid )
-
+      el.setAttribute(FOR_KEY, uid )
       rForMap.set(uid, el.cloneNode(true));
     }
 
     // Clear content within r-for
     el.innerHTML = ''
     
-    // Early return if error
+    // Return if error
     if (!target) return console.warn(`v-for couldn't find binding to proxy.${targetProperty}`)
-    
-    let template = ''
-    
-    if (operator.toLowerCase() == 'of') {
-      for (const value of target) {
-        template += getTemplate(value, el)
-      }
-    }
 
-    // Replace the r-for innerHTML with template
-    el.innerHTML = template
+    // Replace the r-for innerHTML with the parsed template
+    el.innerHTML = getParsedTemplate(operator, target, el)
   })
 }
 
